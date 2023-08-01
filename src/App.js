e52f52eac5b01e770ca28a4166b1d2b800c81114
Rute-r <rute_ke@hotmail.com>
2023-08-01 10:25:27 -0300

    basic game blocks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return <button className="square">X</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
-export default App;
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
